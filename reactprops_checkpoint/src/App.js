ad5083dea4be3dea0534b05f9de6064b24c63cf7
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-06-28 05:31:33 -0400

    Update App.js

--- reactprops_checkpoint/src/App.js
+++ reactprops_checkpoint/src/App.js
@@ -1,9 +1,12 @@
+/* default results will be returned as mentioned in profile.js */
+
 import "./profile/profile"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1> Introduction </h1>
       <Profile />
     </div>
   );
