52ac6b463d12987736fc8e209d81e838531d0df3
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-06-29 11:19:26 -0400

    Update App.js

--- reactprops_checkpoint/src/App.js
+++ reactprops_checkpoint/src/App.js
@@ -1,13 +1,23 @@
-/* default results will be returned as mentioned in profile.js */
-
-import "./profile/profile"
+import Image from "./profile/image.jpg"
+import Profile from "./profile/profile";
 import './App.css';
 
 function App() {
+  function HandleName(FullName) {
+    alert(FullName);
+  }
   return (
     <div className="App">
       <h1> Introduction </h1>
-      <Profile />
+      <Profile FullName="Sherlock Holmes" bio="You know my methods, Watson. When you have eliminated the impossible, whatever remains, however improbable, must be the truth?"
+      profession= "Detective" HandleName={HandleName}
+      >
+           <img
+      style={{ width: "240px", borderRadius: "15px" }}
+      src={Image}
+      alt="Sherlock's pic"
+    />
+  </Profile>
     </div>
   );
 }
